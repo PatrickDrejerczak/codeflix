33ac8091f336ed09fd4422173cc8e8a83db51ad2
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-07-24 15:27:28 +0700

    fetch data and carousel UI improved

--- src/Components/Carousels/SingleCarousel.js
+++ src/Components/Carousels/SingleCarousel.js
@@ -3,33 +3,51 @@ import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react'; import './style.css'
 import './style.css'
-const SingleCarousel = () => {
+const SingleCarousel = ({ popularMovies, popularTV, trending, actionMovies }) => {
+
+    console.log(popularTV);
     return (
         <>
             <Swiper
                 spaceBetween={10}
-                slidesPerView={7}
+                slidesPerView={5}
                 navigation
                 scrollbar={{ draggable: true }}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
                 className="container"
             >
-                <SwiperSlide>
-                    <div>h3</div>
-                    <br></br>
-                    <div>h4</div>
-                    <div>h4</div>
-                </SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
-                <SwiperSlide>Slide 10</SwiperSlide>
+                {trending ? trending.map((show) => {
+                    return <SwiperSlide>
+                        <img src={`https://image.tmdb.org/t/p/original${show.backdrop_path}`}></img>
+                        {show.media_type == "tv" ? <h5>{show.name}</h5> : <h5>{show.title}</h5>}
+
+                    </SwiperSlide>
+                }) : <h3>Loading</h3>}
+                {popularMovies ? popularMovies.map((movie) => {
+
+                    return <SwiperSlide>
+                        <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}></img>
+                        <h5>{movie.title}</h5>
+
+                    </SwiperSlide>
+                }) : <h3>Loading</h3>}
+
+                {popularTV ? popularTV.map((tv) => {
+                    return <SwiperSlide>
+                        <img src={`https://image.tmdb.org/t/p/original${tv.backdrop_path}`}></img>
+                        <h5>{tv.name}</h5>
+                    </SwiperSlide>
+                }) : <h3>Loading</h3>}
+
+
+
+                {actionMovies ? actionMovies.map((movie) => {
+                    return <SwiperSlide>
+                        <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}></img>
+                        <h5>{movie.title}</h5>
+                    </SwiperSlide>
+                }) : <h3>Loading</h3>}
             </Swiper>
 
         </>
--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Carousels from '../../Components/Carousels/Carousels'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
-const HomePage = () => {
+const HomePage = ({popularMovies,popularTV, trending, actionMovies}) => {
     return (
         <>
             <Header />
-            <Carousels/>
+            <Carousels popularMovies={popularMovies} popularTV={popularTV} trending={trending} actionMovies={actionMovies}/>
             <Footer />
         </>
     )
