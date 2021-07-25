064dcac7250e8c55d9eef6eb9db0a050a95b1a19
namnhat1110 <namnhat1110@gmail.com>
2021-07-25 23:39:37 +0700

    add search box to navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
 // let trending =
 // 	'https://api.themoviedb.org/3/trending/all/week?api_key=be052554f80e371720157b837ddf8d48';
 import logo from "./logo.png";
@@ -8,8 +8,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
 	return (
 		<Navbar className="nav-bar" bg="dark" variant="dark" expand="lg">
-			<Link style={{textDecoration:"none"}} as={Link} to={`/`}><Navbar.Brand>
-				<img 
+			<Link style={{ textDecoration: "none" }} as={Link} to={`/`}><Navbar.Brand>
+				<img
 					src={logo}
 					alt=""
 					width="30"
@@ -21,15 +21,19 @@ const Header = () => {
 			</Link>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="me-auto">
+				<Nav className="mr-auto">
 					<Nav.Link href="#trending">Trending Now</Nav.Link>
 					<Nav.Link href="#movie">Movie</Nav.Link>
 					<Nav.Link href="#tv">TV Shows</Nav.Link>
 					<Nav.Link href="#action">Action</Nav.Link>
 					<Nav.Link href="#adventure">Adventure</Nav.Link>
 					<Nav.Link href="#comedy">Comedy</Nav.Link>
-
 				</Nav>
+
+				<Form inline>
+					<FormControl type="text" placeholder="Movie..." />
+					<Button variant="danger" className="mx-1">Search</Button>
+				</Form>
 			</Navbar.Collapse>
 		</Navbar>
 	);
