07e250fa4db7da26f80df64fe4e7490e9be75ac0
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-07-25 12:19:41 +0700

    added Footer

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import MovieDetailPage from "./Page/MovieDetailPage/MovieDetailPage";
 import TVDetailPage from "./Page/TVDetailPage/TVDetailPage";
 import SearchPage from "./Components/SearchPage/SearchPage";
 const myKey = process.env.REACT_APP_API_KEY;
-
 console.log(myKey);
 const App = () => {
   return (
     <>
       <Switch>
-        <Route path="/" exact component={HomePage} />
+        <Route path="/" exact component={HomePage}/>
         <Route path="/movie/:id" component={MovieDetailPage} />
         <Route path="/tv/:id" component={TVDetailPage} />
         <Route path="/search/" component={SearchPage} />
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,70 @@
 import React from 'react'
 import './style.css'
-
+import {
+    Box,
+    Container,
+    Row,
+    Column,
+    FooterLink,
+    Heading,
+  } from "./FooterStyles";
 const Footer = () => {
     return (
         <>
-            Footer
+            <Box>
+     
+      <Container>
+        <Row>
+          <Column>
+            <FooterLink href="#">Aim</FooterLink>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Testimonials</FooterLink>
+          </Column>
+          <Column>
+            <FooterLink href="#">Writing</FooterLink>
+            <FooterLink href="#">Internships</FooterLink>
+            <FooterLink href="#">Coding</FooterLink>
+            <FooterLink href="#">Teaching</FooterLink>
+          </Column>
+          <Column>
+            <FooterLink href="#">Uttar Pradesh</FooterLink>
+            <FooterLink href="#">Ahemdabad</FooterLink>
+            <FooterLink href="#">Indore</FooterLink>
+            <FooterLink href="#">Mumbai</FooterLink>
+          </Column>
+          <Column>
+            <FooterLink href="#">
+              <i className="fab fa-facebook-f">
+                <span style={{ marginLeft: "10px" }}>
+                  Facebook
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-instagram">
+                <span style={{ marginLeft: "10px" }}>
+                  Instagram
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-twitter">
+                <span style={{ marginLeft: "10px" }}>
+                  Twitter
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-youtube">
+                <span style={{ marginLeft: "10px" }}>
+                  Youtube
+                </span>
+              </i>
+            </FooterLink>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
         </>
     )
 }
