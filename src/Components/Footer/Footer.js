ab1461e2f04b1c65a143fb399ab697a327290b6a
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-07-25 13:02:03 +0700

    improve UI

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -16,50 +16,50 @@ const Footer = () => {
       <Container>
         <Row>
           <Column>
-            <FooterLink href="#">Aim</FooterLink>
-            <FooterLink href="#">Vision</FooterLink>
-            <FooterLink href="#">Testimonials</FooterLink>
+            <FooterLink href="#">Audio and Subtitles</FooterLink>
+            <FooterLink href="#">Media Center</FooterLink>
+            <FooterLink href="#">Privacy</FooterLink>
+            <FooterLink href="#">Contact Us</FooterLink>
           </Column>
           <Column>
-            <FooterLink href="#">Writing</FooterLink>
-            <FooterLink href="#">Internships</FooterLink>
-            <FooterLink href="#">Coding</FooterLink>
+            <FooterLink href="#">Audio Description</FooterLink>
+            <FooterLink href="#">Investor Relations</FooterLink>
+            <FooterLink href="#">Legal Notices</FooterLink>
             <FooterLink href="#">Teaching</FooterLink>
           </Column>
           <Column>
-            <FooterLink href="#">Uttar Pradesh</FooterLink>
-            <FooterLink href="#">Ahemdabad</FooterLink>
-            <FooterLink href="#">Indore</FooterLink>
-            <FooterLink href="#">Mumbai</FooterLink>
+            <FooterLink href="#">Help Center</FooterLink>
+            <FooterLink href="#">Jobs</FooterLink>
+            <FooterLink href="#">Cookie Preferences</FooterLink>
           </Column>
           <Column>
             <FooterLink href="#">
-              <i className="fab fa-facebook-f">
+              <i className="fab fa-facebook-square">
+              </i>
                 <span style={{ marginLeft: "10px" }}>
                   Facebook
                 </span>
-              </i>
             </FooterLink>
             <FooterLink href="#">
               <i className="fab fa-instagram">
+              </i>
                 <span style={{ marginLeft: "10px" }}>
                   Instagram
                 </span>
-              </i>
             </FooterLink>
             <FooterLink href="#">
               <i className="fab fa-twitter">
+              </i>
                 <span style={{ marginLeft: "10px" }}>
                   Twitter
                 </span>
-              </i>
             </FooterLink>
             <FooterLink href="#">
               <i className="fab fa-youtube">
+              </i>
                 <span style={{ marginLeft: "10px" }}>
                   Youtube
                 </span>
-              </i>
             </FooterLink>
           </Column>
         </Row>
